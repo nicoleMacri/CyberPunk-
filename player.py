46b13897adbe0876483f0e20cb0277d9f95bb0ca
Nikoleta Makri <mkrnikoleta@gmail.com>
2025-12-16 18:55:27 +0200

    προσπαθεια για animation παικτη προς το παρον δεν δουλευει σωστα

--- player.py
+++ player.py
@@ -2,6 +2,19 @@ import pygame
 from entities import Entities
 from bullet import Bullet
 
+#def load_player_frames(img,frame_width, frame_height):
+#    """
+#    βοηθητική συνάρτηση για την φόρτωση των frames του παίκτη απο sprite sheet.
+#    Επιστρέφει μια λίστα με τα frames.
+#    """
+#    frames = []
+#    sheet_width = img.get_width()
+#
+#    for x in range(0, sheet_width, frame_width):
+#        frame = img.subsurface(pygame.Rect(x, 0, frame_width, frame_height))
+#        frames.append(frame)
+#    return frames
+
 
 class Player(Entities):
     """
@@ -25,6 +38,23 @@ class Player(Entities):
         self.health = 5  # Αρχική υγεία του παίκτη
         self.controls = controls  # Πλήκτρα ελέγχου του παίκτη
 
+        # ----- sprite παικτή -----
+        self.image = pygame.image.load("assets/Cyborg_idle_.png").convert_alpha() # Φόρτωση εικόνας με διαφάνεια
+        self.rect = self.image.get_rect() 
+        self.rect.center =(self.x,self.y) # Κεντράρισμα του rect στην αρχική θέση
+
+        # ----- animation παίκτη -----
+        #self.animations = {
+        #    "idle": load_player_frames(pygame.image.load("assets/Cyborg_idle.png").convert_alpha(), 32, 32),
+        #    "attack": load_player_frames(pygame.image.load("assets/Cyborg_attack3.png").convert_alpha(), 32, 32),
+        #}
+
+        #self.current_animation = "idle"
+        #self.animation_index = 0
+        #self.animation_speed = 0.15  # Ταχύτητα animation
+        #self.image = self.animations[self.current_animation][0] # Αρχική εικόνα animation
+        #self.last_frame_update = pygame.time.get_ticks()
+
     def shooting(self, bullets_group=None):
         now = pygame.time.get_ticks()
         if now - self.last_fire_time < self.fire_delay:
@@ -44,9 +74,10 @@ class Player(Entities):
             bullets_group.add(bullet)
 
         self.last_fire_time = now
-        return bullet
-    
+        #self.set_animation("attack")
         print("Player is shooting!")
+        return bullet
+        
     
     def score_point(self):
         # Λογική για την απόκτηση πόντου
@@ -84,3 +115,33 @@ class Player(Entities):
         if self.health <= 0:
             self.health = 0
             print("player defeated")
+
+ #
+ #         self.current_animation = animation_name
+ #           self.animation_index = 0
+ #           print(f"Player animation changed to: {animation_name}")
+ #           #self.last_frame_update = pygame.time.get_ticks()
+    
+ #   def animate(self):
+ #       now = pygame.time.get_ticks()
+ #       if now - self.last_frame_update < 100:  # Χρόνος μεταξύ καρέ σε milliseconds
+ #           return
+        
+ #           self.last_frame_update = now
+
+ #       frames = self.animations[self.current_animation]
+ #       self.animation_index += 1
+
+ #       if self.animation_index >= len(frames):
+ #           if self.current_animation == "attack":
+ #               self.set_animation("idle")
+ #           self.animation_index = 0
+    
+ #       self.image = frames[int(self.animation_index)]
+
+ #   def update(self):
+        #self.import_handler(SCREEN_WIDTH, bullets_group)
+ #       self.animate()
+
+ #   def draw(self, screen):
+ #       screen.blit(self.image, self.rect)
